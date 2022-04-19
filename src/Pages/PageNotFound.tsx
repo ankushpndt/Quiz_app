5df54d68d12fcb7e179a04042d567112f40175aa
Ankush Sharma <ankushpndt@gmail.com>
2022-04-19 16:05:56 +0530

    feat: added page not found

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -1,8 +1,15 @@
 import "./PageNotFound.css";
+import ghost from "../assets/ghost.png";
 export const PageNotFound = () => {
 	return (
 		<div className="page__not__found">
-			<h1 className="page__not__found__heading">404</h1>
+			<h1 className="page__not__found__heading">
+				4
+				<span>
+					<img src={ghost} alt="ghost.png" className="ghost__image" />
+				</span>
+				4
+			</h1>
 			<h2 className="page__not__found__heading__two">
 				Error: 404 page not found
 			</h2>
